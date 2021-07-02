4fa93067dd0ec69a977e0b8308e4c3b890c1b67f
kimcuongden1712 <kimcuongden1712@gmail.com>
2021-07-02 21:52:56 +0700

    test commit

--- e-web/e-backend/src/main/java/com/eshop/admin/EShopAdminApplication.java
+++ e-web/e-backend/src/main/java/com/eshop/admin/EShopAdminApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EShopAdminApplication {
 
+    /**
+     * Main application EShop Admin
+     * @param args something like that
+     */
     public static void main(String[] args) {
         SpringApplication.run(EShopAdminApplication.class, args);
+
     }
 
 }
